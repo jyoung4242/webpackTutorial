b9732ddb81ad1707c2a923a11f816791d87bd301
Justin Young <justin_dean_young@yahoo.com>
2022-01-03 13:45:07 -0500

    Updated to make TS, CSS, webpack boilerplate

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,3 @@
-import getClasses from "./getClasses"
 import "./styles.css"
 
 console.log("Hello World - index.js")
-getClasses()
-
-const object = { a: "alpha", b: "bravo" }
-const newObj = { ...object, c: "charlie" }
-console.log(newObj)
-console.log(["a", "b", "c"].includes("b"))
